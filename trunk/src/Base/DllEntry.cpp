6491f488a51541ea87f62db5925b3d6e108521b1
tripleslash <michael-bleis@web.de>
2013-07-27 11:02:20 +0200

    formatting

--- trunk/src/Base/DllEntry.cpp
+++ trunk/src/Base/DllEntry.cpp
@@ -10,34 +10,34 @@
 #include "FileManager.h"
 
 #ifdef DEBUG_USE_LOG
-	#include <io.h>
-	#include <fcntl.h>
+#include <io.h>
+#include <fcntl.h>
 
-	// Creates a console window and redirects output (std::cout etc. to it)
-	void SetupDebugConsole() {
-		AllocConsole();
-		SetConsoleTitleW(L"Debug Console");
+// Creates a console window and redirects output (std::cout etc. to it)
+void SetupDebugConsole() {
+	AllocConsole();
+	SetConsoleTitleW(L"Debug Console");
 
-		// set stdout
-		HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-		int nOutputHandle = _open_osfhandle(reinterpret_cast<intptr_t>(hOutput), _O_TEXT);
-		*stdout = *_fdopen(nOutputHandle, "w");
-		setvbuf(stdout, nullptr, _IONBF, 0);
+	// set stdout
+	HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	int nOutputHandle = _open_osfhandle(reinterpret_cast<intptr_t>(hOutput), _O_TEXT);
+	*stdout = *_fdopen(nOutputHandle, "w");
+	setvbuf(stdout, nullptr, _IONBF, 0);
 
-		// set stdin
-		HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
-		int nInputHandle = _open_osfhandle(reinterpret_cast<intptr_t>(hInput), _O_TEXT);
-		*stdin = *_fdopen(nInputHandle, "r");
-		setvbuf(stdin, nullptr, _IONBF, 0);
+	// set stdin
+	HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+	int nInputHandle = _open_osfhandle(reinterpret_cast<intptr_t>(hInput), _O_TEXT);
+	*stdin = *_fdopen(nInputHandle, "r");
+	setvbuf(stdin, nullptr, _IONBF, 0);
 
-		// set sterr
-		HANDLE hError = GetStdHandle(STD_ERROR_HANDLE);
-		int nErrorHandle = _open_osfhandle(reinterpret_cast<intptr_t>(hError), _O_TEXT);
-		*stderr = *_fdopen(nErrorHandle, "w");
-		setvbuf(stderr, nullptr, _IONBF, 0);
+	// set sterr
+	HANDLE hError = GetStdHandle(STD_ERROR_HANDLE);
+	int nErrorHandle = _open_osfhandle(reinterpret_cast<intptr_t>(hError), _O_TEXT);
+	*stderr = *_fdopen(nErrorHandle, "w");
+	setvbuf(stderr, nullptr, _IONBF, 0);
 
-		std::ios::sync_with_stdio();
-	}
+	std::ios::sync_with_stdio();
+}
 #endif
 
 BOOL APIENTRY DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved) {
